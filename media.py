7f43b089683de7463a0a93364490cafdfcb460a2
Marco André Mendes <marcoandre@gmail.com>
2023-04-11 11:07:13 -0300

    programa da média

---
+++ media.py
@@ -0,0 +1,10 @@
+""""Faça um programa que leia 2 notas e imprima a média"""
+
+print("Média")
+p1 = float(input("Nota 1: "))
+p2 = float(input("Nota 2: "))
+
+media = (p1 + p2) / 2
+
+print(f"A média é {media}")
+
